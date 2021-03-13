2d660c7140cef0594c25a74f05bcbf76e11a7703
vishal22.sharma <vishal22.sharma@ril.com>
2021-03-13 19:17:21 +0530

    CollectionView Implemented with Static Data

---
+++ MoviePosters/MoviePosters/Controllers/MPMoviePostersVC.swift
@@ -0,0 +1,34 @@
+//
+//  MPMoviePostersVC.swift
+//  MoviePosters
+//
+//  Created by Vishal22 Sharma on 13/03/21.
+//
+
+import UIKit
+
+class MPMoviePostersVC: UIViewController {
+    let viewModel = MPMoviePostersViewModel()
+    @IBOutlet weak var moviePostersCollectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupViewProperties()
+        
+        // Do any additional setup after loading the view.
+    }
+    func setupViewProperties() {
+        self.moviePostersCollectionView.delegate = viewModel
+        self.moviePostersCollectionView.dataSource = viewModel
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
---
+++ MoviePosters/MoviePosters/Views/MPMoviePostersCell.swift
@@ -0,0 +1,14 @@
+//
+//  MoviePostersCell.swift
+//  MoviePosters
+//
+//  Created by Vishal22 Sharma on 13/03/21.
+//
+
+import UIKit
+
+class MPMoviePostersCell: UICollectionViewCell {
+    static let cellIdentifier = "MPMoviePostersCell"
+    @IBOutlet weak var posterImage: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+}
